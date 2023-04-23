63a4b46e3ef82833c72af743a3bd4288d9c68791
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-23 18:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let radius=document.getElementById("radius");
 let volume=document.getElementById("volume");
+var r=0;
 function volume_sphere() {
-	let r=parseInt(radius.value);
+	r=parseInt(radius.value);
 	r=Math.pow(r,3);
 	r=(4/3)*(22/7)*r;
 	volume.placeholder=r;

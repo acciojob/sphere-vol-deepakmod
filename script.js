c0580bce55cdb93af35cf37357698777624a19f4
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-23 18:15:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let radius=document.getElementById("radius");
 let volume=document.getElementById("volume");
 function volume_sphere() {
-	let r=Math.parseInt(radius.value);
+	let r=parseInt(radius.value);
 	r=Math.pow(r,3);
 	r=(4/3)*(22/7)*r;
 	volume.placeholder=r;
 } 
-
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
